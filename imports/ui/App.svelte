6bb775a705711e4acc213fb33741d0180a7f8198
Chris Ellis <ellis.cs@gmail.com>
2021-01-23 10:39:05 +0000

    feat(sidebar): add sidebar

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,37 +1,15 @@
 <script>
-  import { useTracker } from "meteor/rdb:svelte-meteor-data";
-  import { Resolutions } from "../api/resolutions";
+  import { Router, Link, Route } from "svelte-routing";
+  import MainLayout from "./layouts/MainLayout.svelte";
   import { LoginWindow, Logout } from "meteor/levelup:svelte-accounts-ui";
-  import Resolution from "./resolutions/Resolution.svelte";
-  let newRes = "";
 
-  $: resolutions = useTracker(() => Resolutions.find({}).fetch());
-  $: userId = useTracker(() => Meteor.userId());
+  import { useTracker } from "meteor/rdb:svelte-meteor-data";
 
-  function handleSubmit(event) {
-    Resolutions.insert({
-      title: newRes
-    });
-
-    newRes = "";
-  }
+  export let url = "";
 </script>
 
-<header>
-  <h1 class="bg-blue-500">YO that's fresh</h1>
-
-  {#if $userId}
-    <Logout />
-  {:else}
-    <LoginWindow />
-  {/if}
-
-  <form on:submit|preventDefault={handleSubmit}>
-    <input type="text" placeholder="Add your resolution" bind:value={newRes} />
-    <button>Submit</button>
-  </form>
-</header>
-
-{#each $resolutions as resolution}
-  <Resolution {resolution} />
-{/each}
+<Router {url}>
+  <MainLayout>
+    <Route path="/" />
+  </MainLayout>
+</Router>
